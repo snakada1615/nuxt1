4f7bb7f86e815caf7d7924c9117e1d4e6e8b4d9c
snakada <khf04440@nifty.com>
2019-11-25 22:17:28 +0900

    Add: load image function

---
+++ pages/test.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="container my-2" style="max-width: 540px;">
+	<div class="row">
+		<div class="col-2 my-0 px-3">
+      <img src="~assets/img/no4.png" />
+		</div>
+		<div class="col-10 my-0 px-1">
+			<h5>Commodities feasibility check for XXX</h5>
+		</div>
+	</div>
+</div>
+</template>
